7a2ff229f5b52e20e4b251a9b3bfbfb150a56a25
javangula <nagaraj.ror@gmail.com>
2014-09-25 18:19:51 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmchartsExample::Application.config.secret_token = 'b2b17647fe2177a4cf686942efe7005798e3e6e2d9e2a11c9d7700aaf647719715f3c327c4a2f4f0f91f377ad9aeb9191c9733a2d1f30db5939cc5a478cf3669'
